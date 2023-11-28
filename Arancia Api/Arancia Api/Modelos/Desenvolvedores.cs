d23e8a2e302bbd15104c6f0427f3e37a1aed5281
lucas-arancia <150852650+lucas-arancia@users.noreply.github.com>
2023-11-28 01:17:36 -0300

    Incluindo informações necessarias referentes ao cadastro de desvolvedores

--- Arancia Api/Arancia Api/Modelos/Desenvolvedores.cs
+++ Arancia Api/Arancia Api/Modelos/Desenvolvedores.cs
@@ -1,6 +1,19 @@
-﻿namespace Arancia_Api.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Arancia_Api.Modelos
 {
     public class Desenvolvedores
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Company { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string TelephoneNumber { get; set; }
     }
+
 }
