224f141cd91fbdfecb613c5453c9a8e6aadb95c0
amandademendonca <150849984+amandademendonca@users.noreply.github.com>
2024-01-10 16:41:23 -0300

    Delete Data directory

--- Data/Dto/UpdateDesenvolvedorDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Arancia_Api.Data.Dto;
-
-public class UpdateDesenvolvedorDto
-{
-    [Required(ErrorMessage = "O campo de nome é obrigatório.")]
-    public string Nome { get; set; }
-}
--- Data/Dto/UpdateProjetoDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Arancia_Api.Data.Dto;
-
-public class UpdateProjetoDto
-{
-    [Required]
-    public string Nome { get; set; }
-}
